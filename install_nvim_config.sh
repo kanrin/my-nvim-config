baad2a7f413a25754c12b3902698b89849cf718f
曾翰林 <hl_zeng@droidhang.com>
2023-07-19 10:43:19 +0800

    feat: update installer

--- install_nvim_config.sh
+++ install_nvim_config.sh
@@ -2,32 +2,44 @@
 
 INSTALL_DIR=$HOME/.config/nvim
 
-
+PACKER_DIR=${HOME}/.local/share/nvim/site/pack/packer/start/packer.nvim
+VIM_GO_DIR=${HOME}/.local/share/nvim/site/pack/plugins/start/vim-go
 
 success() {
-  echo -e "[\\033[0;32mOK\\033[0;39m]"
-  exit 0
+    echo -e "[\\033[0;32mOK\\033[0;39m]"
 }
 
 failed() {
-  echo -e "[\\033[0;31mFail\\033[0;39m]"
-  exit 1
+    echo -e "[\\033[0;31mFail\\033[0;39m]"
+    exit 1
+}
+
+pass() {
+    echo -e "[\\033[0;33mPass\\033[0;39m]"
 }
 
 check() {
- if [[ $1 -eq 0 ]]; then
-  success
- else
-  failed
- fi
+    if [[ $1 -eq 0 ]]; then
+        success
+    else
+        failed
+    fi
 }
 
-echo -n "Install Packer\t"
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-check $?
-echo -n "Install vim-go\t"
-git clone https://github.com/fatih/vim-go.git ~/.local/share/nvim/site/pack/plugins/start/vim-go
-check $?
+echo -n "Install Packer    "
+if [ -d ${PACKER_DIR} ]; then
+    pass
+else
+    git clone -q --depth 1 https://github.com/wbthomason/packer.nvim ${PACKER_DIR} 
+    check $?
+fi
+echo -n "Install vim-go    "
+if [ -d ${VIM_GO_DIR} ]; then
+    pass
+else
+    git clone -q https://github.com/fatih/vim-go.git ${VIM_GO_DIR}
+    check $?
+fi
 mkdir -p $INSTALL_DIR
 cp -r nvim/* $INSTALL_DIR
 echo "Complete"
