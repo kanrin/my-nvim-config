1fefa2634a32b4c6af8d9540b1c8f3389d981813
曾翰林 <hanlin@uxin.com>
2026-01-14 11:50:28 +0800

    fix: plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -153,4 +153,52 @@ return {
     "windwp/nvim-ts-autotag",
     ft = { "html", "typescriptreact", "javascriptreact" },
   },
+  {
+    "yetone/avante.nvim",
+    -- ⚠️ 一定要加上这一行配置！！！！！
+    build = vim.fn.has "win32" ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      or "make",
+    event = "VeryLazy",
+    version = false, -- 永远不要将此值设置为 "*"！永远不要！
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      -- 在此处添加任何选项
+      -- 例如
+      provider = "gemini",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- 以下依赖项是可选的，
+      "nvim-telescope/telescope.nvim", -- 用于文件选择器提供者 telescope
+      "hrsh7th/nvim-cmp", -- avante 命令和提及的自动完成
+      "ibhagwan/fzf-lua", -- 用于文件选择器提供者 fzf
+      {
+        -- 支持图像粘贴
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- 推荐设置
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- Windows 用户必需
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- 如果您有 lazy=true，请确保正确设置
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 }
