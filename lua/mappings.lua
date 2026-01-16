ed7315e0ae65ccb942788ebd8241f477bea2866d
曾翰林 <hanlin@uxin.com>
2026-01-16 16:14:44 +0800

    feat: update desc

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,11 +7,11 @@ local map = vim.keymap.set
 map("i", "jk", "<ESC>")
 map("n", "<leader>pvm", ":MarkdownPreview\r")
 map("", "<leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
-map("n", "<leader>ga", ":Git add .\r")
-map("n", "<leader>gm", ":Git commit\r")
-map("n", "<leader>gp", ":Git push\r")
-map("n", "<leader>gs", ":Git pull\r")
-map("n", "<leader>gf", ":Git fetch --all\r")
-
+map("n", "<leader>ga", ":Git add .\r", { desc = "git add ." })
+map("n", "<leader>gm", ":Git commit\r", { desc = "git commit with message" })
+map("n", "<leader>gp", ":Git push\r", { desc = "git push" })
+map("n", "<leader>gs", ":Git pull\r", { desc = "git pull" })
+map("n", "<leader>gf", ":Git fetch --all\r", { desc = "git fetch" })
+map("n", "<leader>gl", ":Git log\r", { desc = "git log" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
